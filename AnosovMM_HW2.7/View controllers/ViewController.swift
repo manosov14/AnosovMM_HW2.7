2da03744fd701ef67741e865332237addba8a121
manosov14 <59836080+manosov14@users.noreply.github.com>
2021-07-26 23:59:59 +0300

    Создана структура проекта

--- AnosovMM_HW2.7/View controllers/ViewController.swift
+++ AnosovMM_HW2.7/View controllers/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Михаил on 26.07.2021.
 //
-
+/Users/mihail/Documents/GitHub/AnosovMM_HW2.7/AnosovMM_HW2.7/ViewController.swift
 import UIKit
 
 class ViewController: UIViewController {
